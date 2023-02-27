652d8fe2d12a69022df512d2ac278cb31b953064
MathaweeRobkhob <6406022620061@fitm.kmutnb.ac.th>
2023-02-27 22:20:35 +0700

    home

--- home.php
+++ home.php
@@ -1,1 +1,43 @@
 <!DOCTYPE html>
+<html lang="en">
+
+    <head>
+
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+
+        <tiktie>NFT Website</title>
+
+        <!-- Bootstrap core css -->
+        <link href="vendor/bootstrap/css/bootatrap.min.css" rel="stylesheet">
+
+        <!-- Additional core css -->
+        <link rel="stylesheet" href="assets/css/fontawesome.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="stylesheet" href="assets/css/owl.css">
+    </head>
+
+    <body>
+        <!-- Header -->
+        <div class="sub-header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-xs-12">
+                        <ul class="left-info">
+                            <li><a href="#"><i class="fa fa-envelope"></i>contact@Ezgg.com</a></li>
+                            <li><a href="#"><i class="fa fa-phone"></i>123-456-7890</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-4">
+                        <ul class="right-icons">
+                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
