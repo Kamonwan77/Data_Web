084661a523d1eb4237e00b8704d875647738d56c
MathaweeRobkhob <6406022620061@fitm.kmutnb.ac.th>
2023-02-27 23:06:44 +0700

    h

--- home.php
+++ home.php
@@ -41,3 +41,36 @@
                 </div>
             </div>
         </div>
+
+        <header class="">
+            <nav class="navbar navbar-expand-lg">
+              <div class="container">
+                <a class="navbar-brand" href="index.php"><h2>NET Store<em> Website</em></h2></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbar">
+                  <span class="navbar-toggler-ion"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                  <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                      <a class="nav-link" href="index.php">Home 
+                        <span class="sr-only">(current)</span>
+                      </a>
+                    </li>
+                    <li class="nav-item active">
+                      <a class="nav-link" href="products.php">Products</a>
+                    </li>
+                    <li class="nav-item active">
+                      <a class="nav-link" href="show.php">Shopping</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="contact.php">Contact Us</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </nav>
+        </header>
+
+        <!-- Page Content -->
+        <!-- Banner Starts Here -->
+        
