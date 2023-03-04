184a712d00fe05ec0cb4fb5e51ec29eb8cdeb87e
Kamonwan77 <s6406022630016@email.kmutnb.ac.th>
2023-03-04 14:15:00 +0700

    b

---
+++ product-details3.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1,shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+        <title>NFT Website</title>
+        <!-- Bootstrap core CSS -->
+        <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Additional cSS Files -->
+        <link rel="stylesheet" href="assets/css/fontawesome.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="stylesheet" href="assets/css/owl.css">
+    </head>
+    <body>
+
+        <div id="preloader">
+            <div class="jumper">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+        <!-- ***** Preloader End ***** -->
+        <!-- Header -->
+        <div class="sub-header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-xs-12">
+                        <ul class="left-info">
+                            <li><a href="#"><i class="fa fa-envelope"></i>contact@Ezgg.com</a></li>
+                            <li><a href="#"><i class="fa fa-phone"></i>contact@Ezgg.com</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-4">
+                        <ul class="right-icons">
+                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <header class="">
+            <nav class="navbar navbar-expand-lg">
+                <div class="container">
+                    <a class="navbar-brand" href="home.php"><h2>NFT Store<em>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbar"></button>
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarResponsive">
+                        <ul class="navbar-nav ml-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="home.php">Home
+                                    <span class="sr-only">(current)</span>
+                                </a>
+                            </li>
+                            <li class="nav-item active">
+                                <a class="nav-link" href="products.php">Products</a>
+                            </li>
+                            <li class="nav-item active">
+                                <a class="nav-link" href="show.php">Shopping</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="contact.php">contact Us</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <!-- Page content -->
+        <div class="page-heading header-text">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h1><small><del><sup>$</sup>2000.00 </del></small> &nbsp; <sup>$</sup>1000.00</h1>
+                        <span>
+                            ชื่อภูครับเป็นคนหวงแฟน
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="services">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-7">
+                        <div>
+                            <img src="assets/images/product4.jpg" alt="" width="500" height="750" class="img-fluid wc-image">
+                        </div>
+                        <br>
+                    </div>
+                    <div class="col-md-5">
+                        <div class="sidebar-item recent-posts">
+                            <div class="sidebar-heading">
+                                <h4>ข้อมูลรูปภาพ</h4>
+                            </div>
+                            <div class="content">
+                                <p>ภูเองครัม</p>
+                            </div>
+                        </div>
+                        <br>
+                        <br>
+                        <h4>Add to Cart</h4>
+                        <br>
+                            <div class="row">
+                                <div class="col-md-6 col-sm-12">
+                                    <div class="form-group">
+                                        <label for="">Quantity</label>
+                                        <input type="text" value="1" required="" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+                                    <div class="form-group">
+                                        <a href="#"class="filled-button">Add to Cart</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                        <br>
+                    </div>
+                </div>
+                <br>
+                <h4>รายละเอียด</h4>
+                <P>เป็นรูปภาพระดับ Rare เป็นภาพของหนุ่มรูปงาม อัดราดรอป 0.25%</p>
+                <br>
+                <p>ถืออาวุธบ่งบออกให้ใครก็ตามที่เข้ามาเห็นรู้ว่าเค้ารักแฟนมาก</p>
+                <p>ถ้าใครเข้าใกล้เเฟนผม ผมเเทงนะเข้าใจไหมครับ</p>
+                <p>มาจับจองกันได้เเล้วครับซื้อไว้สะสม</p>
+                <br>
+                <br>
+                <br>
+            </div>
+        </div>
+        <div class="sub-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <p>
+                            Cryptocurrrency  2020 NFT
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Bootstrap core Javascript -->
+        <script src="vendor/jquery/jquery.min.js"></script>
+        <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <!-- Additional script -->
+        <script src="assets/js/custom.js"></script>
+        <script src="assets/js/owl.js"></script>
+        <script src="assets/js/slick.js"></script>
+        <script src="assets/js/accordions.js"></script>
+        <script language = "text/javascript">
+            cleared[0] = cleared[1] = cleared[2] = 0;
+            function clearField(t){
+            if(! cleared[t.id]){
+                cleared[t.id] = 1;
+                t.value='';
+                t.style.color='#fff';
+                    }
+            }   
+        </script>
+
+    </body>
+</html>
+
+
+
+
+
+
