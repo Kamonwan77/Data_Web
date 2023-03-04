9b9ad213cd6643a540728f7db941371f63de359b
Your Name <rawipornsr19@gamil.com>
2023-03-04 13:05:21 +0700

    tt

--- signin.php
+++ signin.php
@@ -60,33 +60,33 @@
         </div>
         </form>
         <div class="sub-footer">
-    <div class="container">
-        <div class="container">
-            <div class="col-md-12">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-12">
                 <p>
-                    Cryptocurrency  2020 NFT
+                    cryptocurrency @ 2020 NFT
                 </p>
+              </div>
             </div>
-        </div>
-    </div>
+          </div>
         </div>
 
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="vendor/jquery/jquery.min.js"></script>
+        <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-    <script src="assets/js/custom.js"></script>
-    <script src="assets/js/owl.js"></script>
-    <script src="assets/js/slick.js"></script>
-    <script src="assets/js/accordions.js"></script>
-    <script language = "text/Javascipt">
-        cleared[0] = cleared[1] = cleared[2] =0; //set a cleared flag for each field
-          function clearField(t){                  //declaring the array outside of the
-          if(! cleared[t,id]){                     // function makes it static and global
-              cleared[t,id] = 1;  // you could use true and false, but that's more typing
-              t.value='';         // with more chance of typos
-              t.style.color='#fff';
-              }
-          }
+        <script src="assets/js/custom.js"></script>
+        <script src="assets/js/owl.js"></script>
+        <script src="assets/js/slick.js"></script>
+        <script src="assets/js/accordions.js"></script>
+        <script language = "text/Javascipt">
+            cleared[0] = cleared[1] = cleared[2] =0; //set a cleared flag for each field
+            function clearField(t){                  //declaring the array outside of the
+            if(! cleared[t,id]){                     // function makes it static and global
+                cleared[t,id] = 1;  // you could use true and false, but that's more typing
+                t.value='';         // with more chance of typos
+                t.style.color='#fff';
+                }
+            }
         </script>
 </body>
 </html>
